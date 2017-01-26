ba7ec1c515ad851c596b183b36f980e021ecfa66
cristian.schuszter <cschuszter@metabiota.com>
2017-01-26 11:12:25 +0200

    Added a mock class and test, added dependencies to pom

---
+++ src/main/scala/com/cristis/spark/Executor.scala
@@ -0,0 +1,18 @@
+package com.cristis.spark
+
+/** ***********************************************************************
+  * Copyright (c) Metabiota Incorporated - All Rights Reserved
+  * ------------------------------------------------------------------------
+  * This material is proprietary to Metabiota Incorporated. The 
+  * intellectual and technical concepts contained herein are proprietary
+  * to Metabiota Incorporated. Reproduction or distribution of this
+  * material, in whole or in part, is strictly forbidden unless prior 
+  * written permission is obtained from Metabiota Incorporated.
+  * ************************************************************************/
+
+/**
+  * Created by cristian.schuszter on 2017-01-26.
+  */
+class Executor {
+  def buildStr(x: Int): String = s"string $x"
+}
---
+++ src/test/scala/com/cristis/spark/ExecutorSpec.scala
@@ -0,0 +1,28 @@
+package com.cristis.spark
+
+import org.scalatest.{Matchers, WordSpec}
+
+/** ***********************************************************************
+  * Copyright (c) Metabiota Incorporated - All Rights Reserved
+  * ------------------------------------------------------------------------
+  * This material is proprietary to Metabiota Incorporated. The 
+  * intellectual and technical concepts contained herein are proprietary
+  * to Metabiota Incorporated. Reproduction or distribution of this
+  * material, in whole or in part, is strictly forbidden unless prior 
+  * written permission is obtained from Metabiota Incorporated.
+  * ************************************************************************/
+
+/**
+  * Created by cristian.schuszter on 2017-01-26.
+  */
+class ExecutorSpec extends WordSpec with Matchers {
+  "ExecutorSpec.buildStr" when {
+    "passed a valid int" should {
+      "parse the string" in {
+        val exec = new Executor()
+        exec.buildStr(3) shouldBe "string 3"
+        exec.buildStr(15) shouldBe  "string 15"
+      }
+    }
+  }
+}
